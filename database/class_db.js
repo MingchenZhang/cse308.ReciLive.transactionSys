6372dd0e11e71c541e3330e282aa74f26c7ec6f0
Mingchen Zhang <zmc9499@gmail.com>
2017-04-23 17:59:43 -0400

    added class

--- database/class_db.js
+++ database/class_db.js
@@ -2,7 +2,6 @@ const When = require('when');
 const s = global.s;
 
 var classDB = {};
-var recitationDB = {};
 exports.initDatabase = function (readyList) {
     var classDBPath = s.dbPath + "class";
     var classDBReady = When.defer();
@@ -15,26 +14,49 @@ exports.initDatabase = function (readyList) {
             process.exit(1);
         } else {
             console.log('MongodbClient connection to ' + classDBPath + ' has been established');
+
             classDB.classesColl = db.collection('classes');
             classDB.classesColl.createIndex({owner:1});
             classDB.classesColl.createIndex({createdAt:-1});
+            classDB.classesColl.createIndex({numericID:1}, {unique: true});
+
+            classDB.recitationColl = db.collection('recitation');
+            classDB.recitationColl.createIndex({owner:1});
+
             classDBReady.resolve();
         }
     });
 };
 
 //class: name, startDate, endDate, createdAt, owner
-//recitation: numericID, name, startDate, endDate, createdAt, class
+//recitation: numericID, name, startDate, endDate, createdAt, parentClass
 exports.getClassesByOwner = function (owner) {
-    return classDB.classesColl.find({owner}, {sort: [['cratedAt', -1]]});
+    return classDB.classesColl.find({owner}, {sort: [['createdAt', -1]]}).toArray();
 };
 
 exports.addClass = function (name, startDate, endDate, owner) {
-    var numericID
     return classDB.classesColl.insertOne({
         name,
         startDate,
         endDate,
+        createdAt: new Date(),
         owner,
     });
 };
+
+exports.addRecitation = function (name, startDate, endDate, createdAt, parentClass) {
+    var numericID = Math.floor(Math.random()*10000000);
+    return classDB.recitationColl.insertOne({
+        numericID,
+        name,
+        startDate,
+        endDate,
+        createdAt: new Date(),
+        parentClass,
+    });
+};
+
+exports.getRecitationsByClass = function (parentClass) {
+    return classDB.recitationColl.find({parentClass}, {sort: [['createdAt', -1]]}).toArray();
+};
+
