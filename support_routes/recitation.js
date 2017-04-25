4847bcd7cb4a98802bc93bbcb6862f14a052ddea
mingtong wu <shenmizhe1906@gmail.com>
2017-04-24 23:30:09 -0400

    controller waiting for test

--- support_routes/recitation.js
+++ support_routes/recitation.js
@@ -26,99 +26,49 @@ exports.getRoute = function (s) {
     });
 
 
-    router.post('/ajax/add-recitation', jsonParser, function (req, res, next) {
-
-
-        s.classDB.addClass(req.body.name, req.body.startDate, req.body.endDate, req.body.owner).then((response) => {
-            res.send({result: true});
-        }).catch((e) => {
-                if (typeof e == "error") {
-                    res.send({reslut: false, error: e.message});
-                } else {
-                    res.send({result: false, error: "error in class DB add class"});
-                }
+    router.post('/ajax/add-recitation', jsonParser,  (req, res, next)=> {
+        s.classConn.addRecitation(req.body.name, req.body.startDate, req.body.endDate, req.body.createdAt, req.body.class).then((recitation) => {
+            if (recitation) {
+                var privilege = {};
+                privilege[s.userLoginInfo.userID] = ["admin", "slides", "sound_control"];
+                s.classConn.getStudentsByClass(req.body.class).then((response)=>{
+                    response.foreach((student)=>{
+                        privilege[student.googleID] = []
+                    });
+                    Request({
+                        method: 'POST',
+                        url: "http://room.recilive.stream/dispatch_classroom",
+                        json: {
+                            "classNumber": recitation.numericID,
+                            "privilege": privilege,
+                            "name": recitation.name,
+                            "startDate": recitation.startDate,
+                            "endDate": recitation.endDate,
+                            "status": "LIVE"
+                        }
+                    }, (error, response, body) => {
+                        if (error) return res.status(500).send({status: "error", error, statusCode: response.statusCode});
+                        return res.send(body);
+                    });
+                }).catch((e) => {
+                    if (typeof e == "error") {
+                        res.send({result: false, error: e.message});
+                    } else {
+                        res.send({result: false, error: "error in db get students list"});
+                    }
+                });
+            } else {
+                res.send({result:false,error:'no response from database'});
             }
-        );
-        res.send({result: true});
-    })
+        }).catch((e) => {
+            if (typeof e == "error") {
+                res.send({result: false, error: e.message});
+            } else {
+                res.send({result: false, error: "error in class DB add class"});
+            }
+        });
+    });
 
-    public String addRecitation(HttpServletRequest request, HttpServletResponse response, @RequestBody String body) throws ParseException, NoSuchFieldException, EntityNotFoundException, IOException {
-        JSONParser parser = new JSONParser();
-        JSONObject jsonBody = (JSONObject) parser.parse(body);
-        String name = (String) jsonBody.get("name");
-        String classSource = (String) jsonBody.get("class");
-        String startDate = (String) jsonBody.get("startDate");
-        String endDate = (String) jsonBody.get("endDate");
-        String createAt = (String) jsonBody.get("createAt");
-        String className = (String) jsonBody.get("class");
-        JSONObject resultJson = new JSONObject();
-
-
-        Recitation recitation = new Recitation();
-        RecitationInfo recitationInfo = new RecitationInfo();
-        recitationInfo.setName(name);
-        recitationInfo.setOwner(classSource);
-        recitationInfo.setStartDate(startDate);
-        recitationInfo.setEndDate(endDate);
-        recitationInfo.setCreateAt(createAt);
-        Key key = recitation.getClass(className);
-        long id = recitation.getID(key);
-
-
-        JSONObject json = new JSONObject();
-        json.put("classNumber", id);
-        JSONArray instructor_permissions = new JSONArray();
-        instructor_permissions.put("slides");
-        instructor_permissions.put("sound_control");
-        instructor_permissions.put("chat");
-
-        JSONObject role = new JSONObject();
-        role.put("123",instructor_permissions);
-
-        JSONArray student_permissions = new JSONArray();
-        student_permissions.put("chat");
-        role.put("234",student_permissions);
-
-        json.put("privilege", role);
-
-        json.put("name", name);
-        json.put("startDate", startDate);
-        json.put("endDate", endDate);
-        json.put("status", "LIVE");
-
-
-        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-
-        try {
-            HttpPost dispatch_request = new HttpPost("http://room.recilive.stream/dispatch_classroom");
-            StringEntity params = new StringEntity(json.toString());
-            dispatch_request.addHeader("content-type", "application/json");
-            dispatch_request.setEntity(params);
-            CloseableHttpResponse dispatch_response = httpClient.execute(dispatch_request);
-            HttpEntity entity = dispatch_response.getEntity();
-
-            // Read the contents of an entity and return it as a String.
-            String content = EntityUtils.toString(entity);
-            System.out.println(content);
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            httpClient.close();
-        }
-
-
-
-
-        if(key == null) {
-            resultJson.put("result", false);
-        }else {
-            recitation.addRecitation(key, recitationInfo);
-            resultJson.put("result", true);
-            resultJson.put("id", id);
-        }
-        return resultJson.toString();
-    }
 
     return router;
 };
