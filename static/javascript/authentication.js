d53cf48f69f94c19c0039589a0f47552690367e0
JieLiang <jieliang@cs.stonybrook.edu>
2017-04-29 02:12:36 -0400

    fix logout

--- static/javascript/authentication.js
+++ static/javascript/authentication.js
@@ -2,6 +2,12 @@
  * Created by jieliang on 4/5/17.
  */
 
+ function onLoad() {
+     gapi.load('auth2', function () {
+         gapi.auth2.init();
+     });
+ }
+
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function() {
