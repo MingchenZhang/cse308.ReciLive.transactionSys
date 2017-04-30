d6b135d2f2a522c73a298abe15436e7b7ed612eb
JieLiang <jieliang@cs.stonybrook.edu>
2017-04-30 01:28:59 -0400

    https => http

--- static/javascript/live.js
+++ static/javascript/live.js
@@ -41,7 +41,7 @@ function showStudentList() {
 function updateStudentList(students) {
   $(".student-list").empty();
   students.forEach(function(student) {
-    $(".student-list").append("<h4>" + student + "<i class='fa fa-microphone student-microphone'></i></h4>");
+    $(".student-list").append("<h4 onclick=switchMicrophone("+ student.id +")>" + student.name + "<i class='fa fa-microphone student-microphone'></i></h4>");
   });
 }
 
