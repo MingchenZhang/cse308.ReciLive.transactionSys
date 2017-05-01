6e49a6dbbf6ebd1efff8580be3fd47cc2a51dda5
mingtong wu <shenmizhe1906@gmail.com>
2017-04-30 22:13:40 -0400

    blame Mingchen

--- live_modules/classroom.js
+++ live_modules/classroom.js
@@ -93,9 +93,6 @@ exports.getRoute = function (s) {
                             "name": "third slide",
                             "pages": [
                                 {
-                                    "url": ""
-                                },
-                                {
                                     "url": "http://recilive.stream/get_resource?id=59062c9da0a9785420051ad4"
                                 },
                                 {
