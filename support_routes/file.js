03988e639bfdafa70af1ad0084907cb81a177be1
mingtong wu <shenmizhe1906@gmail.com>
2017-05-09 12:59:36 -0400

    delete local pdf and png files after save to mongodb

--- support_routes/file.js
+++ support_routes/file.js
@@ -1,11 +1,11 @@
 var Express = require('express');
-var Busboy = require('busboy');
-var When = require('when');
-var gs = require('./ghost_script_wraper');
-var path = require('path');
-var os = require('os');
-var fs = require('fs');
-
+var Busboy = require('busboy');             //for upload files
+var When = require('when');                 //promise
+var gs = require('./ghost_script_wraper');  //ghostscript
+var path = require('path');                 //modify path
+var os = require('os');                     //get os tmp path
+var fs = require('fs');                     //modify stream
+var rimraf = require('rimraf');             //remove after
 
 exports.getRoute = function (s) {
     var router = Express.Router();
@@ -19,69 +19,82 @@ exports.getRoute = function (s) {
         }
 
         var fields = {};
-        fields.attachmentList = [];
-        var attachmentPromises = [];
-        var boy = new Busboy({
+        fields.attachmentList = [];     //return to front to display added file(s)
+        var attachmentPromises = [];    //attachment list
+        var boy = new Busboy({          //create a new file upload manager
             headers: req.headers,
             limits: {fields: 50, fieldSize: 40 * 1024, files: 100, fileSize: 10 * 1024 * 1024, headerPairs: 1}
         });
-        boy.on('file', function (fieldname, file, filename, encoding, mimetype) {
-            var pdfFile = false;
+        boy.on('file', function (fieldname, file, filename, encoding, mimetype) {       //when file update
             if (filename.length == 0) {
                 writeError(400, 'file is zero byte');
                 return file.pipe(BlackHole());
             }
             if (filename.match(/(.*)\.pdf$/)) {     //if the filename end with pdf
                 var fileHeader = filename.replace(/^(.*)\.pdf$/, '$1');
-                pdfFile = true;
                 var saveTo = path.join(os.tmpDir(), path.basename(fieldname));
-                if (!fs.existsSync(saveTo)){
+                if (!fs.existsSync(saveTo)) {       //make dir if dir not exist
                     fs.mkdirSync(saveTo);
                 }
-                var uploadP = When.promise((resolve, reject) => {
-                    file.pipe(fs.createWriteStream(path.join(saveTo,filename))).once('finish', () => {
-                        gs().getPageNumber(path.join(os.tmpDir() , path.basename(fieldname) , filename), (pageNumber) => {
-                            if (!pageNumber.success) {        // couldn't get page number
-                                return reject();
-                            }
-                            var pages = parseInt(pageNumber.data);
-                            var filePath =path.join(os.tmpDir() , path.basename(fieldname));
-                            gs()
-                                .batch()
-                                .nopause()
-                                .device('png16m')
-                                .output(path.join(filePath, fileHeader + '-%d.png'))
-                                .input(path.join(filePath , filename))
-                                .exec((err, stdout, stderr) => {
-                                    if (err) {            //err in convertion
-                                        return reject();
-                                    }
-                                    var pngsPromiseList = [];
-                                    for (var index = 1; index < pages + 1; index++) {
-                                        let fileID = s.mongodb.ObjectID();
-                                        let fileName = fileHeader + '-' + index + '.png';
-                                        let file = fs.createReadStream(path.join(filePath,fileName));
-                                        fields.attachmentList[index-1]=({name: fileName, id: fileID});
-                                        var uploadStream = s.resourceConn.getResourceFileBucket()
-                                            .openUploadStreamWithId(fileID, fileName, {
-                                                metadata: {},
-                                                contentType: 'image/png'
-                                            });
-                                        pngsPromiseList.push(When.promise((resolve,reject)=>{
-                                            file.pipe(uploadStream).once('finish', function () {
-                                                return resolve();
-                                            });
-                                        }).catch((err)=>{
-                                            console.err(err.message||"err in pngs promise list id: "+fileID+' number:'+index);
-                                        }));
-                                    }
-                                    return When.all(pngsPromiseList).then(()=>{
-                                        return resolve();
-                                    })
-                                });
-                        });
+                var filePath = path.join(saveTo, fileHeader);
+                if (!fs.existsSync(filePath)) {       //make dir if dir not exist
+                    fs.mkdirSync(filePath);
+                }
+                var uploadP = When.promise((resolve, reject) => {       //promise upload all the png
+                    file.pipe(fs.createWriteStream(path.join(filePath, filename))).once('finish', () => {
+                        //get page number from gs with path and filename
+                        gs().getPageNumber(path.join(filePath, filename),
+                            (pageNumber) => {
+                                if (!pageNumber.success) {        // couldn't get page number
+                                    return reject();
+                                }
+                                var pages = parseInt(pageNumber.data);      //get page number for gs
+
+                                gs()            //convert pdf to pngs
+                                    .batch()
+                                    .nopause()
+                                    .device('png16m')
+                                    .output(path.join(filePath, fileHeader + '-%d.png'))
+                                    .input(path.join(filePath, filename))
+                                    .exec((err, stdout, stderr) => {
+                                        if (err) {            //err in convert
+                                            return reject();
+                                        }
+                                        var pngsPromiseList = [];//promise list for all pngs save to mongodb
+                                        for (var index = 1; index < pages + 1; index++) {
+                                            //add png file one by one
+                                            let fileID = s.mongodb.ObjectID();
+                                            let fileName = fileHeader + '-' + index + '.png';
+                                            let file = fs.createReadStream(path.join(filePath, fileName));
+                                            fields.attachmentList[index - 1] = ({name: fileName, id: fileID});
+                                            var uploadStream = s.resourceConn.getResourceFileBucket()
+                                                .openUploadStreamWithId(fileID, fileName, {
+                                                    metadata: {},
+                                                    contentType: 'image/png'
+                                                });
+                                            pngsPromiseList.push(When.promise((resolve, reject) => {
+                                                file.pipe(uploadStream).once('finish', function () {
+                                                    //upload stream to mongodb
+                                                    return resolve();
+                                                });
+                                            }).catch((err) => {
+                                                console.err(err.message || "err in pngs promise list id: " + fileID + ' number:' + index);
+                                            }));
+                                        }
+                                        return When.all(pngsPromiseList).then(() => {
+                                            return resolve();
+                                        })
+                                    });
+                            });
                     });
-                });
+                }).then(() => {
+                    //remove the folder
+                        rimraf(filePath,
+                            function () {
+                                console.log('delete path:' + path.join(os.tmpDir(), path.basename(fieldname)));
+                            });
+                    }
+                );
             } else {
                 var fileID = s.mongodb.ObjectID();
                 var uploadStream = s.resourceConn.getResourceFileBucket()
@@ -120,7 +133,6 @@ exports.getRoute = function (s) {
                 res.send({result: true, files: fields.attachmentList});
             });
         });
-        //boy.once('finish', function() {});
 
         req.pipe(boy);
     });
